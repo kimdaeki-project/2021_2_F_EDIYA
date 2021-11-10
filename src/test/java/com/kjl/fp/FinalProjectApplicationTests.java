58483b25b9438c17e46090d5d70256bde6c3808e
Ji-subin-97 <83721406+Ji-subin-97@users.noreply.github.com>
2021-11-10 18:28:31 +0900

    Initial commit

---
+++ src/test/java/com/kjl/fp/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjl.fp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
