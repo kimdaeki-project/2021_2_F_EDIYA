8e583198d384a3dc89b805d7e0cc67c2ddf62db5
Ji-subin-97 <83721406+Ji-subin-97@users.noreply.github.com>
2021-11-12 15:39:27 +0900

    insert_footer

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,8 +3,7 @@
  */
  
  // 새로고침시 스크롤 초기화
- window.onload = function(){
-	setTimeout(function(){
-	scrollTo(0,0);	
-	}, 100);
-}
+$(window).onload(function(){
+	location.reload();
+	$(".lang_list").css("display","none");
+});
