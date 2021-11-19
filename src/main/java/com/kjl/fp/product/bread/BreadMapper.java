6c8e336d454ac4c2e227a72fc83a6e4bb6804b86
JulyRam <wogjs715@naver.com>
2021-11-19 09:21:46 +0900

    back_change

--- src/main/java/com/kjl/fp/product/bread/BreadMapper.java
+++ src/main/java/com/kjl/fp/product/bread/BreadMapper.java
@@ -1,18 +1,9 @@
 package com.kjl.fp.product.bread;
 
-import org.apache.ibatis.annotations.Mapper;
+public class BreadMapper {
 
-import com.kjl.fp.product.productMapper;
-
-@Mapper
-public interface BreadMapper extends productMapper {
+	public BreadVO selectAll () throws Exception {
+		return null;
+	}
 	
-	//상품리스트
-	public BreadVO selectAll (BreadVO breadVO) throws Exception;
-	
-	//상품 불러오기
-	public BreadVO selectOne (BreadVO breadVO) throws Exception;
-	
-	
-
 }
