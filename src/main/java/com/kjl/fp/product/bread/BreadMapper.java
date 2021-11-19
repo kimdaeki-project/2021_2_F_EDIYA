fb4708e1d61733eb4dedffbbd5a4ec298ccb9c69
JulyRam <wogjs715@naver.com>
2021-11-19 09:07:53 +0900

    proB

--- src/main/java/com/kjl/fp/product/bread/BreadMapper.java
+++ src/main/java/com/kjl/fp/product/bread/BreadMapper.java
@@ -1,5 +1,18 @@
 package com.kjl.fp.product.bread;
 
-public class BreadMapper {
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kjl.fp.product.productMapper;
+
+@Mapper
+public interface BreadMapper extends productMapper {
+	
+	//상품리스트
+	public BreadVO selectAll (BreadVO breadVO) throws Exception;
+	
+	//상품 불러오기
+	public BreadVO selectOne (BreadVO breadVO) throws Exception;
+	
+	
 
 }
--- src/main/java/com/kjl/fp/product/productMapper.java
+++ src/main/java/com/kjl/fp/product/productMapper.java
@@ -1,5 +1,5 @@
 package com.kjl.fp.product;
 
 public interface productMapper {
-
+	
 }
